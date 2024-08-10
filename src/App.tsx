71b8870eb8fc8400b5fb99fac075f9627905cc84
苏征辉 <343196323@qq.com>
2024-08-10 23:46:01 +0800

    🎉 init: 初始化

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import dayjs from "dayjs";
+import Calendar from "./Calendar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Calendar value={dayjs("2024-8-10")} locale="en-US"></Calendar>
+		</div>
+	);
 }
 
 export default App;
